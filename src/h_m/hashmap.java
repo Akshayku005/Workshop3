0e7194832f9ec0819e6e0e59976895d2d2a35d1d
Akshayku005 <akshaysportive@gmail.com>
2022-04-25 10:25:09 +0530

    edited some code- added to show keys

--- src/h_m/hashmap.java
+++ src/h_m/hashmap.java
@@ -6,14 +6,17 @@ public class hashmap {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-HashMap< String , Integer>customer= new HashMap< >();
-customer.put("Akshay", 005);
-customer.put("nandish",006);
-customer.put("KGF", 1);
-customer.containsValue(1);
-System.out.println(customer.containsValue(1));
-customer.clear();
-System.out.println(customer);
+		HashMap< String , Integer>customer= new HashMap< >();
+		//adding key and value
+		customer.put("Akshay", 005);
+		customer.put("nandish",006);
+		customer.put("KGF", 1);
+		customer.containsValue(1);
+		System.out.println(customer.containsKey("Ak"));
+		System.out.println(	customer.containsKey("KGF"));    //printing the key which is contain in the map
+		System.out.println(customer.containsValue(1));        // printing the value of which contain in the map
+		customer.clear();                                                                     //removing all the mapping from a map
+		System.out.println(customer);
 	}
 
 }
